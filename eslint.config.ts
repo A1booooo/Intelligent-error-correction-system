fa8cfc34c1dc762c78c1f7a9c9dc14415617a3f7
A1booooo <2034293341@qq.com>
2025-10-28 12:01:35 +0800

    test

---
+++ eslint.config.ts
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js, prettier: prettierPlugin },
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettierConfig,
+]);
